c3dbc4132d2bc499cea711eaff3120c625b7c689
Day Torres <88747375+day2555@users.noreply.github.com>
2026-01-19 14:40:02 -0600

    Update 2 page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-export const dynamic = "force-dynamic";
 
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { Hero } from "@/components/landing/Hero";
 import { EventGrid } from "@/components/landing/EventGrid";
